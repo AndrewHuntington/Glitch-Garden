8c85460d9e9c6c0ee4669b9159c128648d0b6683
strugglebunny <andrew.huntington@gmail.com>
2019-05-11 10:11:01 +0900

    First Commit
    Sending to Github for version control after a scary accident today!

---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0f, 50f)] [SerializeField] float moveSpeed = 5f;
+    [Range(0f, 720f)] [SerializeField] float rotateSpeed = 360f;
+    [SerializeField] float damage = 50f;
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        //Rick's code (used in Update)
+        //transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(-Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+
+        
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //needed for Text class
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    Text startText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+    
+    private void UpdateDisplay()
+    {
+        startText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+        
+    }
+
+}
