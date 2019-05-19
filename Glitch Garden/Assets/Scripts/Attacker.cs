f0ed7c4c9eaabde7672fcbcc47cb71360d521d3a
strugglebunny <andrew.huntington@gmail.com>
2019-05-19 18:48:54 +0900

    Attackers Striking Target
    -Created public method StrikeCurrentTarget() in the Attacker script to deal damage to defenders.

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,17 +12,39 @@ public class Attacker : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
     }
 
+    // kicks the attacker out of attack animation loop once the game obj disappers
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
+    }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
     }
 
+
+    // switches attacker game object's animation to attack animation
     public void Attack(GameObject target)
     {
         GetComponent<Animator>().SetBool("isAttacking", true);
         currentTarget = target;
+    }
+
+    // called from an event in the Attacker's attack animation
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
