fcd02dc83b91424a3d9b3fb095ec3579c9cf5c7f
strugglebunny <andrew.huntington@gmail.com>
2019-05-21 21:57:46 +0900

    How to be a Loser
    Created a script to handle player health.
    Create a game over screen, and a way to get there when the player's health reaches zero.

---
+++ Glitch Garden/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] int playerHP = 10;
+    Text playerHPText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHPText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        playerHPText.text = playerHP.ToString();
+    }
+
+    public void ReducePlayerHP()
+    {
+        playerHP -= 1;
+        UpdateDisplay();
+
+        if (playerHP <= 0)
+        {
+            FindObjectOfType<LevelLoader>().LoadGameOver();
+        }       
+    }
+}
